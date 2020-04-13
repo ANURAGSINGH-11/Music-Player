5456b6ec1c988554486d654fb48a9017e39add47
Anurag Singh <anuragsingh.cs.it@gmail.com>
2020-04-13 17:28:25 +0530

    Commited for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Track } from 'ngx-audio-player';   
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'musicplayer';
+
+ 
+msaapDisplayTitle = true;
+msaapDisplayPlayList = true;
+msaapageSizeOptions = [2,4,6];
+msaapDisplayVolumeControls = true;
+   
+msaapPlaylist: Track[] = [
+  {
+    title: 'Audio One Title',
+    link: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
+  },
+  {
+    title: 'Audio Two Title',
+    link: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'
+  },
+  {
+    title: 'Audio Three Title',
+    link: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3'
+  },
+ {
+    title: 'Audio Four Title',
+    link: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3'
+  },
+ {
+    title: 'Audio Five Title',
+    link: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3'
+  },
+ {
+    title: 'Audio Six Title',
+    link: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3'
+  },
+]; 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { NgxAudioPlayerModule } from 'ngx-audio-player';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +21,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,  
+    NgxAudioPlayerModule,
+BrowserAnimationsModule,
+MatSliderModule,
+MatToolbarModule,
+MatIconModule,
+MatRadioModule,
+MatCheckboxModule,
+ MatSlideToggleModule,
+ MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
